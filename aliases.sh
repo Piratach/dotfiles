bf4f7113afd654323b3d3215cb31f336969cc571
piratach <piratach@gmail.com>
2020-11-05 23:57:43 +0700

    updated alias master -> main

--- aliases.sh
+++ aliases.sh
@@ -21,4 +21,16 @@ alias gpl="git pull"
 alias gst="git stash"
 alias gstl="git stash list"
 alias glg='git log --graph --oneline --decorate --all'
-alias gitpom="git push -u origin master"
+alias gitpom="git push -u origin main"
+
+### C/C++ Compiler Aliases ###
+# this uses g++ and gcc instead of macOS's clang
+alias g++="g++-9"
+alias gcc="gcc-9"
+
+### Python Aliases ###
+alias python3="python3.6"
+
+### Personal Folder Aliases -- update to fit needs ###
+alias repos="cdf ~/git-repos"
+alias cmu="cdf ~/Desktop/Cosmos/CMU/fall2020"
