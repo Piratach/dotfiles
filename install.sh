55f8e2aae072c68cab2b07b0955fc7a2e7515840
piratach <piratach@gmail.com>
2022-03-14 00:16:37 -0400

    Added installation scripts for vim.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# vim installation
+./scripts/vim-installation.sh
---
+++ scripts/vim-installation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
